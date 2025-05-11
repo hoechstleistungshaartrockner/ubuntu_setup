6565b897f1cc22732c53b3625518f3f2f6fb714f
Christian <202550520+hoechstleistungshaartrockner@users.noreply.github.com>
2025-05-11 22:05:54 +0200

    :tada: initial commit

---
+++ installation_scripts.sh
@@ -0,0 +1,8 @@
+# apt installations
+sudo apt install git
+git config --global user.name "Christian"
+git config --global user.email "202550520+hoechstleistungshaartrockner@users.noreply.github.com"
+git config --global init.defaultBranch main
+sudo apt install vim
+
+# snap store installations
