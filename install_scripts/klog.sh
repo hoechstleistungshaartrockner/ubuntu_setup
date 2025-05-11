2657d18bc7c49fd60fbc3fd7b71fcb9eb48449c3
Christian <202550520+hoechstleistungshaartrockner@users.noreply.github.com>
2025-05-11 22:57:21 +0200

    :sparkles: added klog and obsidian

---
+++ install_scripts/klog.sh
@@ -0,0 +1,5 @@
+# Installation and Configuration of klog
+
+curl -L https://github.com/jotaen/klog/releases/latest/download/klog-linux.zip --output klog-linux.zip
+unzip klog-linux.zip
+sudo mv klog /usr/local/bin/klog
---
+++ install_scripts/obsidian.sh
@@ -0,0 +1,3 @@
+# Installation and Configuration of Obsidian
+
+sudo snap install obsidian --classic
