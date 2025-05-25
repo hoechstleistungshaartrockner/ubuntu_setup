eb0c1e1adb3cd8022afc7069a93a019aaca86fdb
Christian <202550520+hoechstleistungshaartrockner@users.noreply.github.com>
2025-05-25 09:16:01 +0200

    :wrench: updated vscode configs

--- install_scripts/vscode.sh
+++ install_scripts/vscode.sh
@@ -2,3 +2,9 @@
 # ----------------------------------------
 
 sudo snap install code --classic
+
+# Install VSCode extensions
+code --install-extension jakob101.relativepath
+code --install-extension johnpapa.vscode-peacock
+code --install-extension eamodio.gitlens
+code --install-extension tscpp.xit
